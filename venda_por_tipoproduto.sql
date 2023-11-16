208f7d8d11fc8af83a9ecf3ef1544db1a7b6f3df
joao quessada <joaoquessada4@gmail.com>
2023-11-16 09:55:14 -0300

    Query de vendas por tipo de produto

---
+++ venda_por_tipoproduto.sql
@@ -0,0 +1,7 @@
+/*Valor de venda por tipo de produto*/
+/*Essa query deve retornar o valor bruto das vendas agrupadas por tipo de produto*/
+SELECT 
+ProdutoID,
+SUM(Valor) AS Valor 
+FROM FatoDetalhes_DadosModelagem
+GROUP BY ProdutoID;
