a0fd07e90c2954b0d96babc85c2de2d525965ab4
joao quessada <joaoquessada4@gmail.com>
2023-11-16 10:38:06 -0300

    Agrupando por categoria

--- venda_por_tipoproduto.sql
+++ venda_por_tipoproduto.sql
@@ -1,7 +1,9 @@
 /*Valor de venda por tipo de produto*/
-/*Essa query deve retornar o valor bruto das vendas agrupadas por tipo de produto*/
+/*Essa query deve retornar o valor bruto das vendas agrupadas por tipo de produto (categoria)*/
 SELECT 
-ProdutoID,
-SUM(Valor) AS Valor 
-FROM FatoDetalhes
-GROUP BY ProdutoID;
+categoria,
+SUM(fd.Valor) AS Valor 
+FROM FatoDetalhes fd
+LEFT JOIN Produtos p ON fd.ProdutoID = p.ProdutoID
+LEFT JOIN Categoria c ON p.CategoriaID = c.CategoriaID
+GROUP BY CategoriaID;
